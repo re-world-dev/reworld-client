3c922e4e955a43b93c3856b91a1246aae91f03f3
FewerElk <fewerelk@gmail.com>
2023-12-14 19:14:33 +0100

    add a ingame system (or the beginning of) Add a player (or the beginning of)

--- game.py
+++ game.py
@@ -13,6 +13,18 @@ from libs import assetsconf
 import menu
 from libs import richpresence
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 
-grass = load_texture('a')
-floor = Entity(model='cube',texture='grass',Collider='mesh')
+class Game(object):
+    def __init__(self):
+        self.pt = load_texture("assets/player/texture.png")
+        self.player = FirstPersonController(y=2, origon_y=-.5)
+        self.player.position = Vec3(0, 2, 0)
+        grass = load_texture('assets/grass.png')
+        self.floor = Entity(model='plane',texture='grass', collider='box', scale=(100,1,100), texture_scale=(100, 100))
+        self.player_list = []
+
+    def add_player(self):
+        pe = Entity(model="assets/player/test.obj", scale_y=1, texture=self.pt)
+        pe.position = Vec3(20, 2, 20)
+        self.player_list.append(pe)
